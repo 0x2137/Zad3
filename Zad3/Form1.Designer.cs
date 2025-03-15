9fec5ac12d9a4de416b13396ca5fc5e578c0646f
0x2137 <160619965+0x2137@users.noreply.github.com>
2025-03-15 11:20:28 +0100

    init

---
+++ Zad3/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Zad3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            labelCpu = new Label();
+            labelMemory = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            labelCpu.AutoSize = true;
+            labelCpu.Location = new Point(12, 9);
+            labelCpu.Name = "label1";
+            labelCpu.Size = new Size(38, 15);
+            labelCpu.TabIndex = 3;
+            labelCpu.Text = "label1";
+            // 
+            // label2
+            // 
+            labelMemory.AutoSize = true;
+            labelMemory.Location = new Point(12, 39);
+            labelMemory.Name = "label2";
+            labelMemory.Size = new Size(38, 15);
+            labelMemory.TabIndex = 4;
+            labelMemory.Text = "label2";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(221, 66);
+            Controls.Add(labelMemory);
+            Controls.Add(labelCpu);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label labelCpu;
+        private Label labelMemory;
+    }
+}
---
+++ Zad3/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics;
+
+namespace Zad3
+{
+    public partial class Form1 : Form
+    {
+        private PerformanceCounter cpuCounter;
+        private PerformanceCounter availableMemoryCounter;
+        private System.Windows.Forms.Timer updateTimer;
+        private readonly float totalMemory;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            availableMemoryCounter = new PerformanceCounter("Memory", "Available MBytes");
+
+            totalMemory = GetTotalPhysicalMemory();
+
+            updateTimer = new System.Windows.Forms.Timer();
+            updateTimer.Interval = 1000;
+            updateTimer.Tick += UpdateTimer_Tick;
+            updateTimer.Start();
+
+            // za pierwszym razem trzeba skompilować i odpalić .exe jako administrator
+            if (!EventLog.SourceExists("PerformanceMonitor"))
+            {
+                EventLog.CreateEventSource("PerformanceMonitor", "Application");
+            }
+        }
+
+        private void UpdateTimer_Tick(object sender, EventArgs e)
+        {
+            float cpuUsage = cpuCounter.NextValue();
+            float availableMemory = availableMemoryCounter.NextValue();
+            float usedMemory = totalMemory - availableMemory;
+
+            labelCpu.Text = $"Zużycie CPU: {cpuUsage:F2}%";
+            labelMemory.Text = $"Zużycie pamięci: {usedMemory} MB";
+
+            if (cpuUsage > 90)
+            {
+                EventLog.WriteEntry("PerformanceMonitor", $"Zużycie CPU przekroczyło 90%: {cpuUsage:F2}%", EventLogEntryType.Warning);
+            }
+
+
+            if ((usedMemory / totalMemory) * 100 > 80)
+            {
+                EventLog.WriteEntry("PerformanceMonitor", $"Użycie pamięci przekroczyło 80%: {usedMemory:F2} MB", EventLogEntryType.Warning);
+            }
+        }
+
+        private float GetTotalPhysicalMemory()
+        {
+            var computerInfo = new Microsoft.VisualBasic.Devices.ComputerInfo();
+            return computerInfo.TotalPhysicalMemory / (1024 * 1024);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
